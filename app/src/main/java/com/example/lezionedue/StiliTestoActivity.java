36abd90ac24a3ac988761d166709ee88928ba9b5
DESKTOP-F2URIQD\alceo <giovanni.zorzetti@gmail.com>
2019-10-15 16:19:18 +0200

    Aggiunta stili

---
+++ app/src/main/java/com/example/lezionedue/StiliTestoActivity.java
@@ -0,0 +1,14 @@
+package com.example.lezionedue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StiliTestoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stili_testo);
+    }
+}
